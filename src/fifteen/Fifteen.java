d39151fab415386e7316c7dfd9f129a61e26804f
AmandaNadeeshani <nadeeshanihungampala@gmail.com>
2020-04-30 20:58:59 +0530

    add

---
+++ src/fifteen/Fifteen.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fifteen;
+
+/**
+ *
+ * @author Amanda Nadeeshani
+ */
+public class Fifteen {
+
+    public static void main(String[] args) {
+        int age = 17;
+        /*  if(age>18){
+            System.out.println("you can log");    
+        }
+        else{
+            System.out.println(" you can not log 18+ web");
+           if (age>14){
+               System.out.println(" you can log 14+ web");  
+           }else{
+               System.out.println(" sorry!");
+           }
+        }*/
+        /*
+        switch (age) {
+            case 15:
+                System.out.println("your age is fifteen");
+                break;
+            case 16:
+                System.out.println("your age is 16");
+                break;
+            case 17:
+                System.out.println("your age is 17");
+                break;
+            default:
+                System.out.println("I don't know");
+                break;
+        }
+        
+        
+        
+        int apple = 5;
+        System.out.println(apple++);
+        System.out.println(apple);
+        System.out.println(--apple);
+        
+        apple = apple+5;
+        System.out.println(apple);
+        apple +=5;
+        System.out.println(apple);
+        
+        int count = 0;
+        while(count<5){
+            System.out.println(count);
+            count++;
+        }
+        int apple =1;
+        
+        while(apple<=10){
+            System.out.println(apple);
+            apple+=2;
+        }
+        int i= 10;
+        while(i>0){
+            System.out.println(i);
+            i--;
+        }*/
+        
+        /*
+        while(apple<=5){
+            System.out.println(apple  + " Amanda");
+            apple++;
+        }
+        int apple=12;
+        
+      do{
+          System.out.println(apple);
+          apple++;
+      }while(apple<=10);*/
+        
+        for(int count=5; count<20; count+=5){
+            System.out.println(count);
+        }
+    }
+
+}
